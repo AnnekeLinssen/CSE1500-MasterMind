f38c609042f5be3dfbdff3d03448d23e720d34c5
Alves Marinov <alves@Alves-ZBook.local>
2018-11-25 14:07:48 +0200

    Fixed node modules and succesfully started a server at a specific url

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require('express');
+var url = require("url");
+
+var websocket = require("ws");
+var http = require("http")
 
 var app = express();
 
+
+/*
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+*/
+
+var  port = process.argv[2];
+
+/*
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +44,35 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+*/
+/*
+app.use("/", function(req, res){
+  res.sendfile("client/index.html", {root : "./"});
+});
+*/
+var server = http.createServer(app).listen(port);
+
+var  httpPrefix = "<!doctype html><html><head><meta charset=\"UTF-8\"><title>CSE1500_Mastermind</title></head><body><h1>";
+var httpSuffix = "</h1></body></html>";
+
+app.get("/greetme", function (req, res) {
+  var query = url.parse(req.url, true).query;
+  var name = (query["name"] != undefined ) ? query.name : "Anonymous";
+  res.send(httpPrefix + "Greetings, " + name + httpSuffix);
+})
+
+/*
+const wss = new websocket.Server({ server });
+
+wss.on("connection", function (ws) {
+   setTimeout(() => {
+    console.log("Connection State: " + ws.readyState);
+    ws.send("Thanks for the message: -- The Server :) ");
+    ws.close();
+    console.log("Connection state:  " + ws.readyState); 
+   }, 2000);
+});
+
+server.listen(port);
+*/
+//module.exports = app;
