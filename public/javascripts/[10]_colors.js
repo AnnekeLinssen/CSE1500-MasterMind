d74affd3e6862dc216809e8b5ef6f4ab827eb1aa
Anneke Linssen <annekelinssen@outlook.com>
2018-11-25 14:06:42 +0100

    Added functionality for [10]_colors

---
+++ public/javascripts/[10]_colors.js
@@ -0,0 +1,59 @@
+
+/* Constructor */
+function Ten(colorA, colorB, colorC, colorD, colorE,
+    colorF, colorG, colorH, colorI, colorJ) {
+    Ten_colors[0] = colorA;
+    Ten_colors[1] = colorB;
+    Ten_colors[2] = colorC;
+    Ten_colors[3] = colorD;
+    Ten_colors[4] = colorE;
+    Ten_colors[5] = colorF;
+    Ten_colors[6] = colorG;
+    Ten_colors[7] = colorH;
+    Ten_colors[8] = colorI;
+    Ten_colors[9] = colorJ;
+};
+
+// Possibly collapse [4] and [10] if we can get a flexible number of parameters.
+// Also try to assign the array using a for-loop (#askTA)
+
+/* Sets a new color for the given position i.
+Throws an error if i is out of bounds for the array. */
+Ten_colors.prototype.setColori = function(i, newColor){
+    if (i > 9) {
+        throw ("IndexOutOfBoundsException\n" +
+        "The value of the index was " + i + ", but the maximum is 9.");
+    }
+    Ten_colors[i] = newColor;
+};
+
+/* Gets the color for the given position i.
+Throws an error if i is out of bounds for the array. */
+Ten_colors.prototype.getColori = function(i){
+    if (i > 9) {
+        throw ("IndexOutOfBoundsException\n" +
+        "The value of the index was " + i + ", but the maximum is 9.");
+    }
+    return Ten_colors[i];
+};
+
+/* Resets all the colors to standardColor. */
+Ten_colors.prototype.resetAll = function(){
+    for(var i = 0; i <= 9; i++) {
+        Ten_colors.setColori(i, standardColor);
+    }
+};
+
+/* Checks if all ten colors (in order) of the objects are the same.
+Returns true if they are, else it returns false. */
+Ten_colors.prototype.equals = function(Other) {
+    if(Other instanceof Ten_colors){
+        for(var i = 0; i <= 9; i++) {
+            if (!Ten_colors.getColori(i).equals(Other.getColori(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+};
--- public/javascripts/[4]_colors.js
+++ public/javascripts/[4]_colors.js
@@ -1,23 +1,40 @@
 
 /* Constructor */
-function Four_colors(ColorA, ColorB, ColorC, ColorD) {
-    Four_colors[0] = ColorA;
-    Four_colors[1] = ColorB;
-    Four_colors[2] = ColorC;
-    Four_colors[3] = ColorD;
+function Four_colors(colorA, colorB, colorC, colorD) {
+    Four_colors[0] = colorA;
+    Four_colors[1] = colorB;
+    Four_colors[2] = colorC;
+    Four_colors[3] = colorD;
 };
 
-/* Sets a new color for the given position i */
+// Try to construct the array using a forloop (#askTA)
+// function Four_colors(color1, color2, color3, color4) {
+//     for (var i = 0; i <= 3; i ++) {
+//         var colori = "color";
+//         colori = colori + i;
+//     Four_colors[i] = colori;
+// };
+
+/* Sets a new color for the given position i.
+Throws an error if i is out of bounds for the array. */
 Four_colors.prototype.setColori = function(i, newColor){
+    if (i > 3) {
+        throw ("IndexOutOfBoundsException\n" +
+        "The value of the index was " + i + ", but the maximum is 3.");
+    }
     Four_colors[i] = newColor;
 };
 
-/* Gets the color for the given position i */
+/* Gets the color for the given position i.
+Throws an error if i is out of bounds for the array. */
 Four_colors.prototype.getColori = function(i){
+    if (i > 3) {
+        throw ("IndexOutOfBoundsException.\nThe value of the index was " + i + ", but the maximum is 3.");
+    }
     return Four_colors[i];
 };
 
-/* Resets all the colors to the pre specified standard */
+/* Resets all the colors to the pre specified standard. */
 Four_colors.prototype.resetAll = function(){
     for(var i = 0; i <= 3; i++) {
         Four_colors.setColori(i, standardColor);
