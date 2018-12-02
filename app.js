bde91c3f30d277d83df049b746d924226d610052
Alves Marinov <alves@a88-202-160-166.openfiber.nl>
2018-12-02 15:35:28 +0200

    Player Connection Start

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use("/Game", function(req, res){
 
 var server = http.createServer(app);
 
+var connectionID = 0;
+var player = 0;
+var game = 0;
+
 
 const wss = new websocket.Server({ server });
 
@@ -26,13 +30,27 @@ wss.on("connection", function(ws) {
     setTimeout(function() {
         console.log("Connection state: "+ ws.readyState);
         ws.send("Thanks for the message. --Your server.");
-        ws.close();
+        
         console.log("Connection state: "+ ws.readyState);
-    }, 4000);
+        ws.close();
+    }, 1000);
+
+     /*
+     * two-player game: every two players are added to the same game
+     */
+    let con = ws; 
+    let bahur = connectionID++;
+    let playerType =  player++;
+    let games = game++;
+    //websockets[con.id] = currentGame;
+
+    
     
     ws.on("message", function incoming(message) {
         console.log("[LOG] " + message);
     });
+
+    console.log("Player %d placed in game %d as %d", bahur, games, playerType);
 });
 
 server.listen(port);
